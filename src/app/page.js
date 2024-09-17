34c065bdb20c560e1066720242d71b8f1034e00b
KiwiScreams <screamskiwi3@gmail.com>
2024-09-17 20:03:46 +0400

    styles

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Faq from "@/components/faq/Faq";
 import Flower from "@/components/flower/Flower";
-
+import bgImage1 from "../app/assets/images/images/bg-1.png";
+import bgImage2 from "../app/assets/images/images/bg-2.png";
 export default function Home() {
   return (
     <>
+      <Image src={bgImage1} alt="" className="bg" />
+      <Image src={bgImage1} alt="" className="bg-2" />
       <section className="home-section">
         <Flower />
         <Faq />
