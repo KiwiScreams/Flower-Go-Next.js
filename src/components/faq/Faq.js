1dd81dbf5eb1e07c9257a5c7315d4e4858d322ba
KiwiScreams <screamskiwi3@gmail.com>
2024-09-17 19:16:01 +0400

    faq

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,21 +1,42 @@
+"use client";
 import "./Faq.css";
+import { useState } from "react";
 
 const Faq = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleToggle = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
     <div className="faq-container">
       <h2>Frequently Asked Questions</h2>
       <ul>
-        <li>
+        <li
+          className={activeIndex === 0 ? "active" : ""}
+          onClick={() => handleToggle(0)}
+        >
           <h3>Q: What is this FAQ about?</h3>
-          <p>A: This FAQ is about answering common questions.</p>
+          {activeIndex === 0 && (
+            <p>A: This FAQ is about answering common questions.</p>
+          )}
         </li>
-        <li>
+        <li
+          className={activeIndex === 1 ? "active" : ""}
+          onClick={() => handleToggle(1)}
+        >
           <h3>Q: How do I use this FAQ?</h3>
-          <p>A: Simply read through the questions and answers.</p>
+          {activeIndex === 1 && (
+            <p>A: Simply read through the questions and answers.</p>
+          )}
         </li>
-        <li>
+        <li
+          className={activeIndex === 2 ? "active" : ""}
+          onClick={() => handleToggle(2)}
+        >
           <h3>Q: Can I ask more questions?</h3>
-          <p>A: Yes, feel free to ask us anything!</p>
+          {activeIndex === 2 && <p>A: Yes, feel free to ask us anything!</p>}
         </li>
       </ul>
     </div>
