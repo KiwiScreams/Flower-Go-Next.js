e8a54eb0bd106d6a2f28287ad063945038ef65e7
KiwiScreams <screamskiwi3@gmail.com>
2024-09-17 18:53:49 +0400

    menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,16 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import Heart from "../../app/assets/images/icons/Heart.svg";
 import Profile from "../../app/assets/images/icons/Profile.svg";
 import Buy from "../../app/assets/images/icons/Buy.svg";
 import "./Header.css";
+import { useState } from "react";
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       <header className="desktop">
@@ -32,13 +38,16 @@ const Header = () => {
       </header>
       <header className="mobile">
         <h1 className="logo">Flower Go</h1>
-        <nav>
+        <nav className={isOpen ? "nav show" : "nav"}>
           <ul>
             <li className="active">Home</li>
             <li>Red Rose</li>
             <li>Lotus</li>
             <li>Jasmine</li>
             <li>Contact</li>
+            <li>
+              <span onClick={() => setIsOpen(false)}>x</span>
+            </li>
           </ul>
         </nav>
         <ul>
@@ -52,10 +61,10 @@ const Header = () => {
             <Image src={Buy} alt="" />
           </li>
         </ul>
-        <div className="bar-container">
-          <div className="bar1"></div>
-          <div className="bar2"></div>
-          <div className="bar3"></div>
+        <div className="bar-container" onClick={handleToggle}>
+          <div className={`bar1 ${isOpen ? "change" : ""}`}></div>
+          <div className={`bar2 ${isOpen ? "change" : ""}`}></div>
+          <div className={`bar3 ${isOpen ? "change" : ""}`}></div>
         </div>
       </header>
     </>
