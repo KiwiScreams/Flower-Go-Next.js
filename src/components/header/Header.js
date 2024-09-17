f77e9b973211465a75cf57c6fe21a56d0e13b1ce
KiwiScreams <screamskiwi3@gmail.com>
2024-09-17 18:14:49 +0400

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import Heart from "../../app/assets/images/icons/Heart.svg";
 import "./Header.css";
 const Header = () => {
-    return ( 
-        <>hi</>
-     );
-}
- 
+  return (
+    <>
+      <header>
+        <h1>Flower Go</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Red Rose</li>
+            <li>Lotus</li>
+            <li>Jasmine</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <ul>
+          <li>
+            <Image src={Heart} alt="" width={400} height={300} />
+            <Image src={Heart} alt="" width={400} height={300} />
+            <Image src={Heart} alt="" width={400} height={300} />
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
 export default Header;
