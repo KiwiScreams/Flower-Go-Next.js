753b545345adc58b8531d108f657b63c1cb15c0b
KiwiScreams <screamskiwi3@gmail.com>
2024-09-17 19:36:28 +0400

    flower component

--- src/components/flower/Flower.js
+++ src/components/flower/Flower.js
@@ -1,10 +1,27 @@
 import "./Flower.css";
+import Image from "next/image";
+import heartIcon from "../../app/assets/images/icons/Heart.svg";
+import flower from "../../app/assets/images/images/flower.png";
 const Flower = () => {
-    return ( 
-        <>
-        hi flower
-        </>
-     );
-}
- 
+  return (
+    <>
+      <section className="flower-section">
+        <div>
+          <h2>Minimal red tulip flower vase.</h2>
+          <span>$18.99</span>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat quis
+            imperdiet sodales. Add to Cart
+          </p>
+          <button>Add to Cart</button>
+          <Image src={heartIcon} alt="" />
+        </div>
+        <div>
+          <Image src={flower} alt="" />
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default Flower;
